14ceb87be712f1ac3de481942719a3582aab648e
xtr0d3m0n <uchiha.pubudu@gmail.com>
2021-12-05 16:40:24 +0530

    updates

--- web_app/static/js/drawingBoard.js
+++ web_app/static/js/drawingBoard.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded',function()
 {
-    function getPosition(mouseEvent, sigCanvas) {
-        var x, y;
+    function getPosition(mouseEvent, digitCanvas) {
+        let x, y;
         if (mouseEvent.pageX != undefined && mouseEvent.pageY != undefined) {
            x = mouseEvent.pageX;
            y = mouseEvent.pageY;
@@ -10,17 +10,17 @@ window.addEventListener('DOMContentLoaded',function()
            y = mouseEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
         }
 
-        return { X: x - sigCanvas.offsetLeft, Y: y - sigCanvas.offsetTop };
+        return { X: x - digitCanvas.offsetLeft, Y: y - digitCanvas.offsetTop };
     }
 
      function initialize() {
-        var sigCanvas = document.getElementById("board");
-        var context = sigCanvas.getContext("2d");
-        context.strokeStyle = 'White';
-        var is_touch_device = 'ontouchstart' in document.documentElement;
+        let digitCanvas = document.getElementById("board");
+        let context = digitCanvas.getContext("2d");
+        context.strokeStyle = "#00b3ff";
+        let is_touch_device = 'ontouchstart' in document.documentElement;
 
         if (is_touch_device) {
-            var drawer = {
+            let drawer = {
                 isDrawing: false,
                 touchstart: function (coors) {
                     context.beginPath();
@@ -41,11 +41,11 @@ window.addEventListener('DOMContentLoaded',function()
                 }
            };
            function draw(event) {
-                var coors = {
+                let coors = {
                     x: event.targetTouches[0].pageX,
                     y: event.targetTouches[0].pageY
                 };
-                var obj = sigCanvas;
+                let obj = digitCanvas;
 
                 if (obj.offsetParent) {
                     do {
@@ -56,44 +56,63 @@ window.addEventListener('DOMContentLoaded',function()
                 }
                 drawer[event.type](coors);
            }
-            sigCanvas.addEventListener('touchstart', draw, false);
-            sigCanvas.addEventListener('touchmove', draw, false);
-            sigCanvas.addEventListener('touchend', draw, false);
-            sigCanvas.addEventListener('touchmove', function (event) {
+            digitCanvas.addEventListener('touchstart', draw, false);
+            digitCanvas.addEventListener('touchmove', draw, false);
+            digitCanvas.addEventListener('touchend', draw, false);
+            digitCanvas.addEventListener('touchmove', function (event) {
                 event.preventDefault();
             }, false); 
         }
         else {
             $("#board").mousedown(function (mouseEvent) {
-                var position = getPosition(mouseEvent, sigCanvas);
+                let position = getPosition(mouseEvent, digitCanvas);
 
                 context.moveTo(position.X, position.Y);
                 context.beginPath();
                 $(this).mousemove(function (mouseEvent) {
-                    drawLine(mouseEvent, sigCanvas, context);
+                    drawLine(mouseEvent, digitCanvas, context);
                 }).mouseup(function (mouseEvent) {
-                    finishDrawing(mouseEvent, sigCanvas, context);
+                    finishDrawing(mouseEvent, digitCanvas, context);
                 }).mouseout(function (mouseEvent) {
-                    finishDrawing(mouseEvent, sigCanvas, context);
+                    finishDrawing(mouseEvent, digitCanvas, context);
                 });
             });
             $("#clearButton").click(() =>{
-                context.clearRect(0, 0, sigCanvas.width, sigCanvas.height);
+                context.clearRect(0, 0, digitCanvas.width, digitCanvas.height);
             });
             $("#submitButton").click(() =>{
-                console.log("NN Triggered");
+                let pixelData = context.getImageData(0, 0, digitCanvas.width, digitCanvas.height);
+                let x , y = 0;
+                for(let i = 0; i < pixelData.data.length; i +=4)
+                {
+                    if((i % (4)) == 0)
+                    {
+                        x++
+                    }
+                    if((i % (4 * 560)) == 0)
+                    {
+                        x = 0;
+                        y++
+                    }
+                    if(pixelData.data[i] == 0 && pixelData.data[i + 1] == 179 && pixelData.data[i + 2] == 255)// find out if the rgb values corrospond #00b3ff
+                    {
+                        // send data to the nn
+                        //console.log(x,y);
+                    }
+                }
             });
         }
     }
-    function drawLine(mouseEvent, sigCanvas, context) {
-        var position = getPosition(mouseEvent, sigCanvas);
+    function drawLine(mouseEvent, digitCanvas, context) {
+        let position = getPosition(mouseEvent, digitCanvas);
         context.lineTo(position.X, position.Y);
+        context.lineWidth = 20;
         context.stroke();
     }
-    function finishDrawing(mouseEvent, sigCanvas, context) {
-        drawLine(mouseEvent, sigCanvas, context);
+    function finishDrawing(mouseEvent, digitCanvas, context) {
+        drawLine(mouseEvent, digitCanvas, context);
         context.closePath();
-        $(sigCanvas).unbind("mousemove")
+        $(digitCanvas).unbind("mousemove")
                     .unbind("mouseup")
                     .unbind("mouseout");
     }
